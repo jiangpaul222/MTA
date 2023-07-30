3cdab2693666665b9d3e6f86cdadc768926b7723
jiangpaul222 <109158342+jiangpaul222@users.noreply.github.com>
2023-07-30 03:46:55 -0400

    Update front_page.py
    Updated description

--- src/pages/front_page.py
+++ src/pages/front_page.py
@@ -4,8 +4,8 @@ import src.assets
 ## retro code review comment
 
 def app():
-    src.assets.create_header("""MTA Internal Metrics""")
-    st.write("""This page features ridership metrics for internal use only. """)
+    src.assets.create_header("""MTA CBDTP Metrics""")
+    st.write("""This page features evaluation metrics for the Central Business District Tolling Program. """)
     st.write(
         """Landing page to come."""
     )
